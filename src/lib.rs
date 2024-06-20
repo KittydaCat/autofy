4dec5d093239841adc6d51cdf465f7e1c2c96f5b
KittydaCat <mccahey.fuerst@gmail.com>
2024-06-19 18:38:16 -0700

    Init

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use serde_json::Value;
+use async_recursion::async_recursion;
 
 #[derive(Clone, Debug)]
 struct Track {
@@ -53,46 +54,118 @@ impl Client {
 
     }
 
+    async fn request_get(&self, url: &str) -> Value {
+
+        self.client.get(url)
+            .bearer_auth(&self.auth)
+            .send().await.unwrap()
+            .json::<Value>().await.unwrap()
+
+    }
+
     // fn get_auth
 
-    async fn get_public_playlist(&self, name: &str) -> Playlist {
+    async fn get_public_playlist(&self, id: &str) -> Playlist {
 
-        let response = self.client.get(format!("https://api.spotify.com/v1/playlists/{name}"))
-            .bearer_auth(&self.auth).send().await.unwrap();
+        let json = self.request_get(&format!("https://api.spotify.com/v1/playlists/{id}")).await;
 
-        let json = response.json::<Value>().await.unwrap();
 
         // println!("{}", json.to_string());
 
         let name = json.as_object().unwrap().get("name").unwrap().as_str().unwrap().to_string();
 
-        let tracks_json = json.as_object().unwrap().get("tracks").unwrap()
-            .as_object().unwrap()
-            .get("items").unwrap()
-            .as_array().unwrap();
+        let tracks_json = json.as_object().unwrap().get("tracks").unwrap();
 
-        let tracks = tracks_json.iter().filter_map(|track_json| {
+        let tracks = self.get_tracks(tracks_json).await;
 
-            let track = track_json.as_object().unwrap()
-                .get("track").unwrap()
-                .as_object()?;
+        // let total_items = dbg!(tracks_json.get("total").unwrap()).as_u64().unwrap();
+        //
+        // let mut tracks_items_json = tracks_json.get("items").unwrap()
+        //     .as_array().unwrap();
+        //
+        // let mut tracks = Vec::new();
+        //
+        // loop {
+        //     for track_json in tracks_items_json {
+        //         if let Some(track) = track_json.as_object().unwrap()
+        //             .get("track").unwrap().as_object() {
+        //             let artists = track.get("artists").unwrap()
+        //                 .as_array().unwrap()
+        //                 .iter()
+        //                 .map(|x| x.as_object().unwrap().get("name").unwrap().as_str().unwrap().to_string())
+        //                 .collect();
+        //
+        //             tracks.push(Track {
+        //                 name: track.get("name").unwrap().as_str().unwrap().to_string(),
+        //                 artists,
+        //             })
+        //         }
+        //     }
+        //
+        //     if tracks.len() as u64 != total_items {
+        //
+        //         response = self.client.get(dbg!(tracks_json.get("next").unwrap().as_str()
+        //             .expect("if this fails it means there is less items than expected in the vec but there isnt another page")))
+        //             .bearer_auth(&self.auth).send().await.unwrap();
+        //
+        //         json = response.json::<Value>().await.unwrap();
+        //
+        //         // tracks_json = dbg!(json.as_object().unwrap()).get("tracks").unwrap()
+        //         //     .as_object().unwrap();
+        //
+        //         tracks_items_json = json.get("items").unwrap()
+        //             .as_array().unwrap();
+        //
+        //     } else {
+        //         break
+        //     }
+        //
+        // }
+        
+        Playlist {
+            name,
+            tracks,
+        }
 
+    }
+
+    #[async_recursion]
+    async fn get_tracks(&self, tracks_json: &Value) -> Vec<Track> {
+
+        let items_json = tracks_json.get("items").unwrap().as_array().unwrap();
+
+        let mut tracks: Vec<_> = items_json.iter().filter_map(|track_json| {
+            let track = track_json.get("track").unwrap().as_object()?;
             let artists = track.get("artists").unwrap()
                 .as_array().unwrap()
                 .iter()
-                .map(|x| x.as_object().unwrap().get("name").unwrap().as_str().unwrap().to_string())
+                .map(|x| dbg!(dbg!(x).get("name").unwrap()).as_str().unwrap_or("Unknown Artist").to_string())
                 .collect();
 
             Some(Track {
                 name: track.get("name").unwrap().as_str().unwrap().to_string(),
                 artists,
             })
-        }).collect::<Vec<Track>>();
-        
-        Playlist {
-            name,
-            tracks,
+
+        }).collect();
+
+        if tracks.len() as u64 != dbg!(tracks_json.get("total").unwrap()).as_u64().unwrap() {
+
+            if let Some(next) = tracks_json.get("next")
+                .expect("if this fails it means there is less items than expected in the vec but there isnt another page")
+                .as_str() {
+                tracks.append(&mut self.get_tracks(&self.request_get(next).await).await)
+            }
+
+            else {
+            //     why???
+
+                panic!()
+            }
+
         }
+
+        tracks
 
     }
 
